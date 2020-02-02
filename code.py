c5d5959e548d1448a74655e263933fd8c29b9e88
FoamyGuy <foamyguy@gmail.com>
2020-02-02 10:11:52 -0600

    adding lsm303dlh to the example

--- code.py
+++ code.py
@@ -1,11 +1,17 @@
 import time
 import board
 import busio
-import adafruit_lis3mdl
 from math import atan2, degrees
 
 i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_lis3mdl.LIS3MDL(i2c)
+
+# Uncomment the import and initializer for your magnetometer.
+
+# import adafruit_lis3mdl
+# sensor = adafruit_lis3mdl.LIS3MDL(i2c)
+
+import adafruit_lsm303dlh_mag
+sensor = adafruit_lsm303dlh_mag.LSM303DLH_Mag(i2c)
 
 def vector_2_degrees(x, y):
     radians = atan2(y, x)
